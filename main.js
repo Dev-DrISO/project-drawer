3159b4e7525ed97d466c2c9d29dcf9e9564dbdc2
Dev-DrISO <dr.iso1995@gmail.com>
2025-07-04 16:38:48 -0300

    Definido que o valor Minimo tem que ser maior que o valor Maximo

--- main.js
+++ main.js
@@ -4,8 +4,11 @@ function generateNumber(){
     const max = Math.floor(document.querySelector(".max").value)
     const result = document.querySelector(".result")
 
-    const result1 = Math.floor(Math.random() * (max - min + 1)) + min;
-
-    result.textContent = `Resultado ${result1}`
+    if(min >= max){
+        alert("O valor minimo tem que ser MAIOR que o valor maximo !")
+    }else{
+        const result1 = Math.floor(Math.random() * (max - min + 1)) + min;
+        result.textContent = `Resultado ${result1}`
+    }
 
 }
